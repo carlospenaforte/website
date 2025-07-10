c0d0861ad64679ef0f3f617fddb38c25a53184fa
carlospenaforte <carlosedu_1132@hotmail.com>
2025-07-10 14:30:53 -0300

    added button file. Styling in index.css

--- reactviteproject/src/App.jsx
+++ reactviteproject/src/App.jsx
@@ -1,11 +1,11 @@
+import Button from "./Button";
 import Card from "./Card";
 
 function App() {
     return (
         <>
             <Card/>
-            <Card/>
-            <Card/>
+            <Button/>
         </>
     );
 }
