6211ac3a1737237fb4671e2aa0a6155b495a2fd2
carlospenaforte <carlosedu_1132@hotmail.com>
2025-07-14 14:58:41 -0300

    calling studen file in app.jsx

--- reactviteproject/src/App.jsx
+++ reactviteproject/src/App.jsx
@@ -1,9 +1,11 @@
 import Button from "./Button/Button";
 import Card from "./Card";
+import Student from "./Student"
 
 function App() {
     return (
         <>
+            <Student name="Sponge" />
             <Card/>
             <Button/>
         </>
