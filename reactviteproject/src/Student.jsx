59a2588102c6417cb13db573e07a08dd92ebc28c
carlospenaforte <carlosedu_1132@hotmail.com>
2025-07-14 15:15:16 -0300

    proptypes in student

--- reactviteproject/src/Student.jsx
+++ reactviteproject/src/Student.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types"
 
 function Student(props){
 
@@ -8,6 +9,11 @@ function Student(props){
             <p>Student: {props.isStudent ? "Yes" : "No"}</p>
         </div>
     );
+}
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool,
 }
 
 export default Student
