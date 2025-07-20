ce14670973d238df541594bf71c865261b3050b5
carlospenaforte <carlosedu_1132@hotmail.com>
2025-07-20 15:42:05 -0300

    adding id and price for array fruits and configuration of exibition of the list

--- reactviteproject/src/List.jsx
+++ reactviteproject/src/List.jsx
@@ -1,11 +1,15 @@
 
 function List(){
 
-    const fruits = ["apple", "orange", "banana"];
+    const fruits = [{id: 1, name: "apple", price: "$ 1"},
+                    {id: 2, name: "orange", price: "$ 4"},
+                    {id: 3, name: "banana", price: "$ 2"}];
 
     fruits.sort();
 
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    const listItems = fruits.map(fruit => <li key={fruit.id}>
+                                            {fruit.name}: &nbsp;
+                                            <b>{fruit.price}</b></li>);
 
 
 
