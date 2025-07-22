ad5d84641ce7197b508c670c58988a02fc429392
carlospenaforte <carlosedu_1132@hotmail.com>
2025-07-22 15:47:06 -0300

    usestate for buttons.
    counter button- increment, reset and decement button.
    
    style with css of the counter button.

--- reactviteproject/src/App.jsx
+++ reactviteproject/src/App.jsx
@@ -3,11 +3,14 @@ import Card from "./Card";
 import Student from "./Student"
 import UserGreeting from "./UserGreeting";
 import List from "./List.jsx"
+import MyComponent from "./MyComponent.jsx";
+import Counter from "./Counter.jsx";
 
 function App() {
     return (
         <>
-            <Button />
+            <MyComponent />
+            <Counter /> 
         </>
     );
 }
